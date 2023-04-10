6f50bda9169b4392bcd19aef909a118af6ff25e1
duy <duy211099@gmail.com>
2023-04-10 11:07:54 +0700

    feat: creations section

--- loopstudios/tailwind.config.js
+++ loopstudios/tailwind.config.js
@@ -13,11 +13,14 @@ module.exports = {
                 lightBlue: 'hsl(233, 100%, 69%)',
                 darkGrayishBlue: 'hsl(210, 10%, 33%)',
                 grayishBlue: 'hsl(201, 11%, 66%)',
-              },
-              fontFamily: {
+            },
+            fontFamily: {
                 sans: ['Josefin Sans', 'sans-serif'],
                 alata: ['Alata']
-              },
+            },
+            letterSpacing: {
+                widest: ".3em"
+            }
         },
     },
     plugins: [],
